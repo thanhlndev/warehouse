eee661a12063875e7f4e916fc50235c0c63d8d54
thanhln <thanhln.dev@gmail.com>
2025-01-30 16:21:18 +0700

    config securiry among identity-service and profile-service 2

--- identity-service/src/main/java/cloud/thanhln/identity/service/UserService.java
+++ identity-service/src/main/java/cloud/thanhln/identity/service/UserService.java
@@ -1,6 +1,5 @@
 package cloud.thanhln.identity.service;
 
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import cloud.thanhln.identity.constant.PredefinedRole;
 import cloud.thanhln.identity.domain.Role;
