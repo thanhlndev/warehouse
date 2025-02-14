68dc71c07434aa40d1970d399001da139ab504a6
thanhln <thanhln.dev@gmail.com>
2025-02-14 20:48:01 +0700

    add initial post-service

---
+++ post-service/src/main/java/cloud/thanhln/post/exception/AppException.java
@@ -0,0 +1,21 @@
+package cloud.thanhln.post.exception;
+
+public class AppException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+	public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    private ErrorCode errorCode;
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
---
+++ post-service/src/main/java/cloud/thanhln/post/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package cloud.thanhln.post.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
+    ;
+    private final int code;
+    private final String message;
+    private final HttpStatusCode httpStatusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode httpStatusCode) {
+        this.code = code;
+        this.message = message;
+        this.httpStatusCode = httpStatusCode;
+    }
+}
