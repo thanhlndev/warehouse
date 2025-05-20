05ef77287829eb6491eb73dac4955a7ef2a878bf
thanhln.dev <thanhln.dev@gmail.com>
2025-05-20 23:50:13 +0700

    rollback web-app

--- web-app/app/(auth)/register/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react"
-export default function RegisterLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return children
-}
-
--- web-app/app/transactions/loading.tsx
+++
@@ -1,4 +0,0 @@
-export default function Loading() {
-  return <div className="flex items-center justify-center p-8">Đang tải...</div>
-}
-
